7054eab34e338e1c09476e7970e7e3fda8482bd3
eugene_zaharch <eugenezaharchenko@gmail.com>
2023-12-01 23:14:42 +0200

    Makefile, Dockerfile implemented

---
+++ cmd/kbot.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
+var appVersion string = "v1.0.1"
+
+var kbotCmd = &cobra.Command{
+	Use:     "kbot",
+	Aliases: []string{"start"},
+	Short:   "A brief desc of command",
+	Long:    "The long desc of a command",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("kbot %s started", appVersion)
+		kbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please, go AWAY! And check %s", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			return err
+		})
+
+		kbot.Start()
+
+	},
+}
